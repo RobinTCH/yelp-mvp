c6987400db0c5a0b7b111b36486fa7cda1f7e1f2
robin chane <robin.chane@gmail.com>
2021-08-11 16:37:12 +0200

    yelp mvp without css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurant_list = [
+  [ "very bad", "rue de la paix", "italian", "068249523001" ],
+  [ "bad", "un trou de verdure", "japanese", "03695236002" ],
+  [ "average", "paris intra-muros", "french", "026881662003"],
+  [ "not bad", "bordeaux", "belgian", "0638862004"],
+  [ "good", "tout sauf paris", "chinese", "01265488005"]
+]
+
+restaurant_list.each do |name, address, category, phone_number|
+  Restaurant.create( name: name, address: address, category: category, phone_number: phone_number)
+end
